fbc5385af0b05058fe41d9040f3cfd8bd58a30b4
Teo <ferraricorneloup.teo@gmail.com>
2023-04-03 19:35:07 +0200

    buttons generate from json and text appears correctly

---
+++ Assets/Assets/Custom/Scripts/FamilyButtonScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class FamilyButtonScript : MonoBehaviour
+{
+    private TMP_Text textSelf;
+    private TMP_Text textTarget;
+    public string question = "question";
+    public string answer = "answer";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textTarget = GameObject.Find("AnswerText").GetComponent<TMP_Text>();
+        textSelf = GetComponentInChildren<TMP_Text>();
+        textSelf.text = question;
+        Button btn = GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        textTarget.text = answer;
+    }
+}
--- Assets/Assets/Custom/Scripts/FamilyReader.cs
+++ Assets/Assets/Custom/Scripts/FamilyReader.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class FamilyReader : MonoBehaviour
 {
+
+    [SerializeField]
+    public GameObject prefabButton;
+
+    [SerializeField]
+    public GameObject content;
+
     // Start is called before the first frame update
     void Start()
     {
         JsonParsing parser = GameObject.Find("ScenarioReader").GetComponent<JsonParsing>();
         Discussion[] values = parser.rootObject.scenario.family.discussion;
-        GameObject content = GameObject.Find("content");
+        
 
-
+    
         foreach (Discussion d in values) {
-            print(d.question);
+            if (d.ID != "default") {
+                GameObject button = Instantiate(prefabButton);
+                button.GetComponent<FamilyButtonScript>().answer = d.answer;
+                button.GetComponent<FamilyButtonScript>().question = d.question;
+                button.transform.parent = content.transform;
+            }
         }
     }
 }
